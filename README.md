94f0060337db941ce7c10edb3883b564de75b311
Kevin Wiesmüller <kwiesmueller@seibert-media.net>
2018-03-06 18:50:19 +0100

    init

---
+++ README.md
@@ -0,0 +1,46 @@
+# dummyMail
+[![Go Report Card](https://goreportcard.com/badge/github.com/seibert-media/dummyMail)](https://goreportcard.com/report/github.com/seibert-media/dummyMail)
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Build Status](https://travis-ci.org/seibert-media/dummyMail.svg?branch=master)](https://travis-ci.org/seibert-media/dummyMail)
+[![Docker Repository on Quay](https://quay.io/repository/seibertmedia/dummyMail/status "Docker Repository on Quay")](https://quay.io/repository/seibertmedia/dummyMail)
+
+A bot for making discord server democratic.
+
+## Description
+
+## Dependencies
+This project has a pretty complex Makefile and therefore requires `make`.
+
+Go Version: 1.8
+
+Install all further requirements by running `make deps`
+
+## Usage
+
+```
+dummyMail
+```
+
+## Development
+
+This project is using a [basic template](github.com/playnet-public/gocmd-template) for developing PlayNet command-line tools. Refer to this template for further information and usage docs.
+The Makefile is configurable to some extent by providing variables at the top.
+Any further changes should be thought of carefully as they might brake CI/CD compatibility.
+
+One project might contain multiple tools whose main packages reside under `cmd`. Other packages like libraries go into the `pkg` directory.
+Single projects can be handled by calling `make toolname maketarget` like for example:
+```
+make template dev
+```
+All tools at once can be handled by calling `make full maketarget` like for example:
+```
+make full build
+```
+Build output is being sent to `./build/`.
+
+If you only package one tool this might seam slightly redundant but this is meant to provide consistence over all projects.
+To simplify this, you can simply call `make maketarget` when only one tool is located beneath `cmd`. If there are more than one, this won't do anything (including not return 1) so be careful.
+
+## Contributions
+
+Pull Requests and Issue Reports are welcome.
---
+++ cmd/dummyMail/dummyMail.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"runtime"
+	"syscall"
+
+	"github.com/playnet-public/libs/log"
+
+	flag "github.com/bborbe/flagenv"
+
+	raven "github.com/getsentry/raven-go"
+	"github.com/golang/glog"
+	"github.com/kolide/kit/version"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+const (
+	app    = "dummyMail"
+	appKey = "dummyMail"
+)
+
+var (
+	maxprocsPtr = flag.Int("maxprocs", runtime.NumCPU(), "max go procs")
+	sentryDsn   = flag.String("sentrydsn", "", "sentry dsn key")
+	dbgPtr      = flag.Bool("debug", false, "debug printing")
+	versionPtr  = flag.Bool("version", true, "show or hide version info")
+
+	sentry *raven.Client
+)
+
+func main() {
+	flag.Parse()
+
+	if *versionPtr {
+		fmt.Printf("-- //S/M %s --\n", app)
+		version.PrintFull()
+	}
+	runtime.GOMAXPROCS(*maxprocsPtr)
+
+	// prepare glog
+	defer glog.Flush()
+	glog.CopyStandardLogTo("info")
+
+	var zapFields []zapcore.Field
+	// hide app and version information when debugging
+	if !*dbgPtr {
+		zapFields = []zapcore.Field{
+			zap.String("app", appKey),
+			zap.String("version", version.Version().Version),
+		}
+	}
+
+	// prepare zap logging
+	log := log.New(appKey, *sentryDsn, *dbgPtr).WithFields(zapFields...)
+	defer log.Sync()
+	log.Info("preparing")
+
+	var err error
+
+	// prepare sentry error logging
+	sentry, err = raven.New(*sentryDsn)
+	if err != nil {
+		panic(err)
+	}
+	err = raven.SetDSN(*sentryDsn)
+	if err != nil {
+		panic(err)
+	}
+	errs := make(chan error)
+
+	// catch system interrupts
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	// catch errors and throw fatal //TODO: is this good?
+	go func() {
+		ret := <-errs
+		if ret != nil {
+			log.Fatal(ret.Error())
+		}
+	}()
+
+	// run main code
+	log.Info("starting")
+	raven.CapturePanicAndWait(func() {
+		if err := do(log); err != nil {
+			log.Fatal("fatal error encountered", zap.Error(err))
+			raven.CaptureErrorAndWait(err, map[string]string{"isFinal": "true"})
+			errs <- err
+		}
+	}, nil)
+	log.Info("finished")
+}
+
+func do(log *log.Logger) error {
+	return nil
+}
